ac61f99f2129cc9a386e037a0592d954b01c4130
amtakeuchi <95510069+amtakeuchi@users.noreply.github.com>
2025-05-31 22:14:21 -0500

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,11 @@
 import Hero from "./Hero";
 import About from "./About";
-import ContactPage from "./ContactPage";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <About />
-      <ContactPage />
     </>
   );
 };
